bda431b3080de995e0184e2aa653f7ee8f2a4142
shakil6527 <shakilmamun.4350gmail.com>
2023-11-21 23:27:30 +0600

    first commit

---
+++ app.js/app.js
@@ -0,0 +1,24 @@
+// 1st Question Answer
+const feedTime = "noon";
+if (feedTime === "morrning") {
+  console.log("It's morning and time to feed the animals.");
+} else if (feedTime === "afternoon") {
+  console.log("It's afternoon and time to feed the animals.");
+} else {
+  console.log("You can't feed the animals now.");
+}
+
+// 2nd Questions Answer
+const maximumWeight = 500;
+const passengersWeight = 450;
+if (passengersWeight <= maximumWeight) {
+  console.log("Passengers can cross the river safely.");
+} else {
+  console.log("Passengers can't cross the river.");
+}
+
+//  3rd Questions Answer
+const myName = "Shakil";
+const myHobby = "reading books";
+const sentence = `Hi, my name is ${myName}. My favorite hobby is ${myHobby}.`;
+console.log(sentence);
